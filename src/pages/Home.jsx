0077218cd03d1460fb357104c2c05bd3e67ce604
ChrisLam790 <christopherlam790@gmail.com>
2025-12-17 15:45:54 -0500

    Add stylings & themes; Prepare pag layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
 import { Diff } from "lucide-react";
 
 export const Home = () => {
-  return <div> Home Page</div>;
+  return (
+    <div className="min-h-screen">
+      {/* Theme Toggle */}
+
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
 };
