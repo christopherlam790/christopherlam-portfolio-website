246895262c39a419ef6700b1d07aaef3a88b68c8
ChrisLam790 <christopherlam790@gmail.com>
2025-12-17 14:43:19 -0500

    Fix imports; Establish page routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import { Diff } from "lucide-react";
+
+export const Home = () => {
+  return <div> Home Page</div>;
+};
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+import { Diff } from "lucide-react";
+
+export const NotFound = () => {
+  return <div> Not Found </div>;
+};
