ab0e2a25d4f276be8762bb9c2af64a0f0d21c2f6
ChrisLam790 <christopherlam790@gmail.com>
2025-12-17 21:19:08 -0500

    Meteor background animation

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 
 export const StarBackground = () => {
   const [stars, setStars] = useState([]);
+  const [meteors, setMeteors] = useState([]);
 
   useEffect(() => {
     generateStars();
+    generateMeteors();
   }, []);
 
   /*
@@ -32,6 +34,28 @@ export const StarBackground = () => {
     setStars(newStars);
   };
 
+  /*
+   * Randomly generate meteors
+   */
+  const generateMeteors = () => {
+    const numberOfMeteors = 4;
+
+    const newMeteors = [];
+
+    for (let i = 0; i < numberOfMeteors; i++) {
+      newMeteors.push({
+        id: i,
+        size: Math.random() * 2 + 1,
+        x: Math.random() * 100,
+        y: Math.random() * 20,
+        delay: Math.random() * 15,
+        animationDuration: Math.random() * 3 + 3,
+      });
+    }
+
+    setMeteors(newMeteors);
+  };
+
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
       {stars.map((star) => (
@@ -45,6 +69,21 @@ export const StarBackground = () => {
             top: `${star.y}%`,
             opacity: star.opacity,
             animationDuration: `${star.animationDuration}s`,
+          }}
+        />
+      ))}
+
+      {meteors.map((meteor) => (
+        <div
+          key={meteor.id}
+          className="meteor animate-meteor"
+          style={{
+            width: `${meteor.size}px`,
+            height: `${meteor.size}px`,
+            left: `${meteor.x}%`,
+            top: `${meteor.y}%`,
+            animationDelay: meteor.delay,
+            animationDuration: `${meteor.animationDuration}s`,
           }}
         />
       ))}
