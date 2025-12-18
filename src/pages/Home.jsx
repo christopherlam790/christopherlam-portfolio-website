49b08c9dd6b3bfef0ba14a26fef83ec2c304262f
ChrisLam790 <christopherlam790@gmail.com>
2025-12-18 11:58:19 -0500

    Nav bar implementation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   return (
