3ff99dc13d5149ca8b9f9f906558e68b0afd1ced
ChrisLam790 <christopherlam790@gmail.com>
2025-12-18 14:34:19 -0500

    Finish hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,5 @@
+import { ArrowDown } from "lucide-react";
+
 export const HeroSection = () => {
   return (
     <section
@@ -12,7 +14,23 @@ export const HeroSection = () => {
               Christopher Lam
             </span>
           </h1>
+
+          <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+            I’m a CS graduate passionate about turning data into insights. I
+            build data analysis and visualization projects that make complex
+            information clear and actionable.
+          </p>
+
+          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+            <a href="#projects" className="custom-button">
+              View My Work
+            </a>
+          </div>
         </div>
+      </div>
+      <div className="absolute bottom-8 left-1/2 transform -translat-x-1/2 flex flex-col items-center animate-bounce">
+        <span className="text-sm ext-muted-foreground mb-2"> Scroll</span>
+        <ArrowDown className="h-5 w-5 text-primary" />
       </div>
     </section>
   );
